461f1fdc44c87808bfdd967381747acbb0695546
Cesar Esquivel <czr.esquivel@gmail.com>
2022-05-18 21:20:58 -0500

    personal website

--- src/About.js
+++ src/About.js
@@ -5,14 +5,14 @@ function App() {
     return (
         <div className="content2">
 
-            <div className="my-image">
+            <div className="my-image" height={900} width={900}> 
                 <img src={headshot} className="headshot"
-                     alt="./assets/headshot.jpg"></img>
+                     alt="./assets/headshot.jpg" height={300} width={300}></img>
 
             </div>
             <div className="description">
                 <div className="header1">
-                    <h1> About ME</h1>
+                    <h1> About Me</h1>
                 </div>
                 <div className="header2">
                     <p>Hello! I am Cesar and I work with HTML/CSS and I am
